4a843f9a3839fdb4fefbf5eb038961d728965a14
RanaWaqar <rana.waqar@arbisoft.com>
2023-07-03 17:53:22 +0500

    f2

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import { configureStore } from 'Stores';
 
 
 console.log('my name is waqar');
+console.log('my name is waqar 2');
 const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
